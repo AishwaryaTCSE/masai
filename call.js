2439207f6293dfacfd51b6d9ebe13aed6fff62ff
AishwaryaTCSE <159999677+AishwaryaTCSE@users.noreply.github.com>
2025-04-24 17:27:38 +0530

    Add files via upload

---
+++ call.js
@@ -0,0 +1,12 @@
+
+function personInfo() {
+    console.log("Name: " + this.name + ", Age: " + this.age);
+  }
+  
+  let person = {
+    name: "Alice",
+    age: 22
+  };
+  
+  personInfo.call(person);
+  
